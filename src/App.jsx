8ed7a88ba02be36c2f5d6a28bbd30cfae59ed91e
KingSlayer3X <alito321a@gmail.com>
2025-09-13 23:09:34 +0300

    react render-props training

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
-import Mouse from "./mouse";
+const DataFetcher = ({render, url}) => {
+    const [data, setData] = useState([]);
+    useEffect(()=> {
+        if (url.includes("desserts")){
+            setData(["cake","ice cream", "pie", "brownie"]);
+        } else {
+            setData(["water", "soda", "juice", "Cola" ,"MilkShake"]);
+        }
+    },[]);
+    return render(data);
+};
 
+const DessertsCount = () => {
+    return (
+        <DataFetcher
+        url="https://littlelemon/desserts"
+        render={(data) => <p>{data.length} desserts</p>}
+        />
+    );
+};
+
+const DrinksCount = () => {
+    return(
+        <DataFetcher
+        url="https://littlelemon/drinks"
+        render={(data) => <h3>{data.length} Drinks</h3>}
+        />
+    )
+}
 export default function App(){
     return (
         <>
-        <Mouse />
+        <div className="App">
+            <header className="Header">Little Resturant</header>
+            <DessertsCount />
+            <DrinksCount />
+        </div>
         </>
-    )
+    );
 }
